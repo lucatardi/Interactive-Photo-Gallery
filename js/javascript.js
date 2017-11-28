9e2eba9e46cbd614db789ae0cc7712c5a16faca9
Luca tardito <lucatardi@gmail.com>
2017-11-28 16:48:43 +0000

    insert second function

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,22 @@
 $(document).ready(function() {
-    $("#lightgallery").lightGallery();
+  $("#lightgallery").lightGallery();
 });
 
-$("#lightGallery").lightGallery({
-  mode: "lg-slide-circular"
+$("#lightgallery").lightGallery({
+  mode: "lg-fade"
+});
+
+$('#search').on("keyup change", function() {
+  input = document.getElementByID("search");
+  word = input.value.toUpperCase();
+  linkA = document.getElementByClassName("images");
+  for(var count = 0; count < linkA.length; count++) {
+    image = linkA[count].children();
+    if (image.alt.toUpperCase().indexOf(word) === 0) {
+      $(linkA[count].children()).attr("display","none");
+    }
+    else {
+      $(linkA[count].children()).attr("display"," ");
+    }
+  }
 });
